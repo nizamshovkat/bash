0711f93ee02d7e388aa529c7316925eb944b8baf
nizamshovkat <nizamgit9@gmail.com>
2024-07-17 10:10:28 +0500

    update

--- rezrf.sh
+++ rezrf.sh
@@ -1,6 +1,10 @@
-#/bin/bash
-SOURCE_DIR="/home/shovkat/text.txt"
-BACKUP_DIR="/home/shovkat/backup"
+#!/bin/bash
+
+echo "Путь до файла:"
+read SOURCE_DIR
+
+echo "Путь куда надо копировать:"
+read BACKUP_DIR
 
 echo "Starting backup from $SOURCE_DIR to $BACKUP_DIR"
 cp -r $SOURCE_DIR $BACKUP_DIR
